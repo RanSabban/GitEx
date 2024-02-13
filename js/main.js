1b8c543f660f07a7cadc295ba1f01b78cb6facff
Ran Sabban <sabbanran@gmail.com>
2024-02-13 18:26:07 +0200

    Add fourth ball click

--- js/main.js
+++ js/main.js
@@ -34,5 +34,25 @@ function onThirdBallClick(){
     ballEl2.style.width = ballSize1 + 'px'
     ballEl2.style.height = ballSize1 + 'px'
     ballEl2.innerText = ballSize1
-
 }
+
+function onFourthdBallClick(){
+    var rdmnewSize1 = getRandomIntInclusive(20,60)
+    var rdmnewSize2 = getRandomIntInclusive(20,60)
+    const ballEl1 = document.querySelector('.ball')
+    const ballEl2 = document.querySelector('.ball2')
+    const sizeBall1 = ballEl1.offsetWidth - rdmnewSize1
+    const sizeBall2 = ballEl2.offsetWidth - rdmnewSize2
+    console.log(sizeBall1,sizeBall2);
+    if (sizeBall1 > 100){
+        ballEl1.style.width = sizeBall1 + 'px'
+        ballEl1.style.height = sizeBall1 + 'px'
+        ballEl1.innerText = sizeBall1
+    }
+    if (sizeBall2 > 100){
+        ballEl2.style.width = sizeBall2 + 'px'
+        ballEl2.style.height = sizeBall2 + 'px' 
+        ballEl2.innerText = sizeBall2
+    }
+}
+
