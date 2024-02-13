56fbe40dbd627db6589e624ef7ac4f1edb13294f
Ran Sabban <sabbanran@gmail.com>
2024-02-13 18:09:11 +0200

    Add third ball click swap between the other two balls

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ function onBallClick(ballEl,maxDiameter){
     ballEl.innerText = size
     ballEl.style.backgroundColor = getRandomColor() 
 }
+
+function onThirdBallClick(){
+    const ballEl1 = document.querySelector('.ball')
+    const ballEl2 = document.querySelector('.ball2')
+    const ballComputedStyle1 = window.getComputedStyle(ballEl1)
+    const ballComputedStyle2 = window.getComputedStyle(ballEl2)
+    const ballColor1 = ballComputedStyle1.backgroundColor
+    const ballColor2 = ballComputedStyle2.backgroundColor
+    ballEl1.style.backgroundColor = ballColor2
+    ballEl2.style.backgroundColor = ballColor1
+    const ballSize1 = ballEl1.offsetWidth
+    const ballSize2 = ballEl2.offsetWidth
+    ballEl1.style.width = ballSize2 + 'px'
+    ballEl1.style.height = ballSize2 + 'px'
+    ballEl1.innerText = ballSize2
+    ballEl2.style.width = ballSize1 + 'px'
+    ballEl2.style.height = ballSize1 + 'px'
+    ballEl2.innerText = ballSize1
+
+}
