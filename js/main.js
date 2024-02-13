524df11640ce470853ae22436b7a505f1eb542aa
Ran Sabban <sabbanran@gmail.com>
2024-02-13 16:49:36 +0200

    Add random size each click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 'use strict'
 
 function onBallClick(ballEl){
-    var size = ballEl.offsetWidth + 50
-    if (size === 450) size = 100
+    var size = ballEl.offsetWidth
+    if (size >= 400) {
+        size = 100
+        ballEl.style.width = size + 'px'
+        ballEl.style.height = size + 'px'
+        ballEl.innerText = size   
+        return 
+    }
+    size += getRandomIntInclusive(20,60)
     ballEl.style.width = size + 'px'
     ballEl.style.height = size + 'px'
     ballEl.innerText = size
