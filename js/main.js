e93d5ad8376d09a679f83ce58668d0c9b836f1d1
Ran Sabban <sabbanran@gmail.com>
2024-02-13 15:39:52 +0200

    Add limit js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 function onBallClick(ballEl){
     var size = ballEl.offsetWidth + 50
+    if (size === 450) size = 100
     ballEl.style.width = size + 'px'
     ballEl.style.height = size + 'px'
     ballEl.innerText = size
